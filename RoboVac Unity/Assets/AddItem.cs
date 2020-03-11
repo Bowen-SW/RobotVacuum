b9590fe75254adcae1a8c7f468c59b1e6a5f9abb
Thomas Mosser <32948870+tom0002@users.noreply.github.com>
2020-03-11 18:45:23 -0500

    Added Item Button Object Instatiation
    Added a script to the Furniture Menu item buttons that will enable someone to specify what GameObject that will be instatiated when the button is pressed.

---
+++ RoboVac Unity/Assets/AddItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddItem : MonoBehaviour
+{
+    public Button button;
+    public GameObject objectToAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        Instantiate(objectToAdd, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+    }
+
+}
