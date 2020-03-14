33cea22d677ff3b75935d43a0f331290e7289cee
Thomas Mosser <32948870+tom0002@users.noreply.github.com>
2020-03-14 18:51:12 -0500

    Made another metric crap ton of changes
    Gonna be honest...I have no idea what in the world were all of the changes I made here. Imma head out.

---
+++ RoboVac Unity/Assets/Scripts/ViewReportButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewReportButton : MonoBehaviour
+{
+    public GameObject menu;
+    public Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowReportMenu()
+    {
+        if (GameObject.FindGameObjectWithTag("SimulationReportMenu") == null)
+        {
+            GameObject newObj = Instantiate(menu, new Vector3(canvas.transform.position.x, canvas.transform.position.y, canvas.transform.position.z), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+            newObj.transform.SetParent(canvas.transform, false);
+            RectTransform objRectTrans = newObj.GetComponent<RectTransform>();
+            objRectTrans.offsetMin = new Vector2(0.0f, 0.0f);
+            objRectTrans.offsetMax = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
