f1888cb6bff9188c5314dd73b611850bed4206df
T.J. Mosser <tjmosser@gmail.com>
2020-03-26 17:15:18 -0500

    Added Test Script for Managing Temporary Menus

--- RoboVac Unity/Assets/Scripts/NewFloorPlanMenuManager.cs
+++ RoboVac Unity/Assets/Scripts/NewFloorPlanMenuManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NewFloorPlanMenuManager : MonoBehaviour
 {
     public GameObject menu;
 
+    public TextMeshProUGUI fileName;
+    public TextMeshProUGUI roomWidth;
+    public TextMeshProUGUI roomHeight;
+    public TextMeshProUGUI carpetType;
+
+    public TestSimScript simScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        simScript = GetComponentInParent<TestSimScript>();
     }
 
     // Update is called once per frame
@@ -18,8 +26,19 @@ public class NewFloorPlanMenuManager : MonoBehaviour
         
     }
 
+    public void SendData()
+    {
+        //Debug.Log(fileName.text);
+        //Debug.Log(roomWidth.text);
+        //Debug.Log(roomHeight.text);
+        //Debug.Log(carpetType.text);
+
+    }
+
     public void Close()
     {
+        simScript.SaySomething(fileName.text);
+
         Destroy(menu);
     }
 }
