94202560c7f694a25b1d8889f076a08e6c8b01a2
T.J. Mosser <tjmosser@gmail.com>
2020-04-06 17:13:28 -0500

    Update for Adam

--- RoboVac Unity/Assets/Scripts/SaveLoad.cs
+++ RoboVac Unity/Assets/Scripts/SaveLoad.cs
@@ -114,6 +114,7 @@ public class SaveLoad : MonoBehaviour
             {
                 appendedNum++;
                 fileName = "floorPlan"+ appendedNum.ToString();
+                UserInputInformation.FileNameGS = fileName;
             }        
         }
         else
